6e1c0848fff458821ff3fb322cb1e42fc3d6602b
Reik Loeber <reik.loeber@gmail.com>
2025-09-02 16:59:32 +0200

    Setup script adapted - If key already exists

--- setup_prerequisites.sh
+++ setup_prerequisites.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 ###################################################################
 # Setup script                                                    #
@@ -6,32 +6,71 @@
 # Config of SSH and GIT                                           #
 ###################################################################
 
-# Check if 2 arguments are given
+# Check if 3 arguments are given
 # If not, the script will stop
-if [ "$#" -ne 2 ]
+if [ "$#" -ne 3 ]
 then
-  echo "You have to input the Account-Name and IP of the V-Server"
-  echo "ex: ./setup_prerequisites.sh bob 1.2.3.4"
+  echo "Something went wrong!!!"
+  echo "You have to input 3 Paramaters:"
+  echo "Account-Name, IP and environment"
+  echo "ex: ./setup_prerequisites.sh bob 1.2.3.4 dev"
   exit 1
 fi
 
 USERNAME=$1
-VSERVER=$2 
+VSERVER=$2
+ENVIRONMENT=$3
 CONN=${USERNAME}@${VSERVER}
+SSH_KEY_FOLDER=~/.ssh/da_vserver
+FILE=$SSH_KEY_FOLDER/da_vserver_"$ENVIRONMENT"
 
+if ! test -d "$SSH_KEY_FOLDER"; then
+  echo "Create folder to store the keys..."
+  mkdir -p SSH_KEY_FOLDER
+fi
 ###################################################################
 # SSH                                                             #
 ###################################################################
 
-# SSH-KEY creation amd storing in USERHOME/.ssh/da_vserver folder
-# Folder is created if not exists
-echo "Create SSH-KEY for $USERNAME ..."
-mkdir -p ~/.ssh/da_vserver
-ssh-keygen -t ed25519 -N "" -f ~/.ssh/da_vserver/da_vserver
+# SSH-KEY creation and upload
 
-# Upload the SSH-KEY to the V-SERVER
-echo "Upload SSH-KEY to $VSERVER ..."
-ssh-copy-id -i  ~/.ssh/da_vserver/da_vserver.pub -o StrictHostKeyChecking=no "${CONN}"
+confirm() {
+    while true; do
+        read -p "$1 (y/n): " -n 1 -r
+        echo    # new line
+        case $REPLY in
+            [Yy] ) return 0;;
+            [Nn] ) return 1;;
+            * ) echo "Invalid response.";;
+        esac
+    done
+}
+
+create_ssh_key(){
+    ssh-keygen -t ed25519 -N "" -f $SSH_KEY_FOLDER/da_vserver_"$1"
+}
+
+upload_ssh_key(){
+  echo "Upload SSH-KEY to $VSERVER ..."
+  ssh-copy-id -i  $SSH_KEY_FOLDER/da_vserver_"$ENVIRONMENT".pub -o StrictHostKeyChecking=no "${CONN}"
+}
+
+if test -f "$FILE"; then
+    echo "SSH key pair already exists. The keys are being overwritten."
+    if confirm "Do you want to continue?"; then
+        echo "Overwrite keys..."
+        /bin/rm -f "$FILE"
+        /bin/rm -f "$FILE.pub"
+        create_ssh_key "$ENVIRONMENT"
+        upload_ssh_key  
+    else
+    echo "Cancelled."
+    fi
+else
+    echo "Create SSH-KEY for environment: $ENVIRONMENT..."
+    create_ssh_key "$ENVIRONMENT"
+    upload_ssh_key
+fi
 
 # Replace placeholder for V-SERVER IP in inventory-File
 # Creates a backup of original inventory file
@@ -41,7 +80,7 @@ sed -i'.bak' "s/#VSERVER-IP/$VSERVER/g" ansible/inventory.yml
 # Replace placeholder for V-SERVER IP and USERNAME in ssh_config-File
 # Creates a backup of original ssh_configuration file
 echo "Configure ssh_configuration file ..."
-sed -i'.bak' -e "s/#VSERVER-IP/$VSERVER/g" -e "s/#USERNAME/$USERNAME/g" ssh_configuration
+sed -i'.bak' -e "s/#VSERVER-IP/$VSERVER/g" -e "s/#USERNAME/$USERNAME/g" -e "s/#KEYPATH/$SSH_KEY_FOLDER/da_vserver_$ENVIRONMENT/g" ssh_configuration
 
 # Include the new configuration file in config of ssh
 # Creates a backup of original config file"
